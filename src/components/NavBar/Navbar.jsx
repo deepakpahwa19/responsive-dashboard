a1b0ed2e333f687271d15e2ee395b71886142e1f
dpahawa <deepak_pahawa@intuit.com>
2021-03-27 21:08:06 +0530

    asdfsdf

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,28 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Nav, NavbarContainer, NavLogo } from '../../views/NavBarView/NavbarView';
+import { Nav, NavbarContainer } from '../../views/NavBarView/NavbarView';
 
 export const Navbar = () => {
     return (
         <Nav>
             <NavbarContainer>
                 <h1 to='/'>ABC Engineering of College</h1>
-                <Button>
-                    <Anchor href='src/assets/assignment.postman_collection.json' download>
-                        Download Postman Collection
-                    </Anchor>
-                </Button>
+                <Anchor href='src/assets/assignment.postman_collection.json' download>
+                    Download Postman Collection
+                </Anchor>
             </NavbarContainer>
         </Nav>
     );
 };
 
-const Button = styled.button`
-    padding: 0.35rem 0.6rem;
-    background-color: aqua;
-`;
-
 const Anchor = styled.a`
     color: '#fff';
     text-decoration: none;
+    padding: 0.35rem 0.6rem;
+    background-color: aqua;
 `;
